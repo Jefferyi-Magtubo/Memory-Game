972ef2e1e404b6f80861441a4fc35e65d372ac1b
jffryi <jefferyi.magtubo@gmail.com>
2024-03-31 14:04:51 -0500

    Ironed out styling on circle on different cases, add logic when currently selected circles hits 2

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,14 +1,18 @@
 import React from "react"
+import { nanoid } from 'nanoid'
 
-export default function Circle({character, clicked, theme, changeClick, index} : {character: string | number, clicked: boolean, theme : string, changeClick : Function, index: number}) {
-    console.log(character)
+export default function Circle({character, clicked, theme, changeClick, index, customKey, correctPairs} : {character: string | number, clicked: boolean, theme : string, changeClick : Function, index: number, customKey: string, correctPairs: string[]}) {
+
     return (
-        <button className={`flex justify-center items-center ${clicked ? "bg-lightBlue2" : "bg-lightGray "}  ${theme === 'numbers' ? "text-xl p-4" : "p-6 py-4"} rounded-full`}
+        <button key={customKey} className={`${character} flex justify-center items-center 
+        ${clicked ? "bg-darkYellow " : "bg-darkGray text-darkGray"} 
+        ${correctPairs.includes(character.toString()) ? 'bg-lightGray cursor-not-allowed pointer-events-none' : ''}  
+        ${theme === 'numbers' ? "text-xl p-4" : "p-6 py-4"} rounded-full`}
         onClick={() => changeClick(index)}>
             {theme === 'icons' ?
-            <i className={`fa-brands fa-${character} text-white text-lg`}></i>
+            <i className={`fa-brands fa-${character} text-lg`}></i>
             :
-            <h1 className="text-white font-extrabold">{character}</h1>}
+            <h1 className=" font-extrabold">{character}</h1>}
         </button>
     )
 }
