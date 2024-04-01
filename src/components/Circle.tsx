fa766e3155dfea3bb05155d509ce5f85bf33ca09
jffryi <jefferyi.magtubo@gmail.com>
2024-04-01 11:23:17 -0500

    Added logic to find the winners of the games,

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -5,9 +5,9 @@ export default function Circle({character, clicked, theme, changeClick, index, c
 
     return (
         <button key={customKey} className={`${character} flex justify-center items-center 
-        ${clicked ? "bg-darkYellow " : "bg-darkGray text-darkGray hover:bg-gray-500 hover:text-gray-500"} 
+        ${clicked ? "bg-darkYellow pointer-events-none" : "bg-darkGray text-darkGray hover:bg-gray-500 hover:text-gray-500"} 
         ${correctPairs.includes(character.toString()) ? 'bg-lightGray cursor-not-allowed pointer-events-none ' : ''}  
-        ${theme === 'numbers' ? "text-xl p-4 " : "p-6 "}
+        ${theme === 'numbers' ? "text-xl p-4 " : "p-4 "}
         ${currentSelected.length === 2 ? 'pointer-events-none' : ''}
         rounded-full`}
         onClick={() => changeClick(index)}>
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,13 +1,13 @@
 
-export default function Player({player, score, currentPlayer} : {player : number, score: number, currentPlayer : number}) {
+export default function Player({player, score, currentPlayer} : {player : number, score: { [key: number]: number }, currentPlayer : number}) {
 
 
     return(
         <>
             
-            <div className={`${currentPlayer === player ? "bg-darkYellow" : "bg-playerCardDefault"} p-4 w-16 flex flex-col items-center mr-4 font-athl player`}>
+            <div className={`${currentPlayer === player ? "bg-darkYellow md:current" : "bg-playerCardDefault"} p-4 w-16 flex flex-col items-center mr-4 font-athl player relative`}>
                 <h1 className={` ${currentPlayer === player ? "text-white" : "text-lightBlue"} font-bold`}>P{player}</h1>
-                <h1 className={` ${currentPlayer === player ? "text-white" : "text-darkGray"} font-bold` }>{score}</h1>
+                <h1 className={` ${currentPlayer === player ? "text-white" : "text-darkGray"} font-bold` }>{score[player]}</h1>
             </div>
         </>
 
