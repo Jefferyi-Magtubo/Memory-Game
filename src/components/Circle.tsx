e60e42d7fb8fddab90d31fc421d5b948593559cf
jffryi <jefferyi.magtubo@gmail.com>
2024-04-01 23:00:17 -0500

    Finalized game logic, made circles a consistent height and width

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -4,7 +4,7 @@ import { nanoid } from 'nanoid'
 export default function Circle({character, clicked, theme, changeClick, index, customKey, correctPairs, currentSelected, gridSize} : {character: string | number, clicked: boolean, theme : string, changeClick : Function, index: number, customKey: string, correctPairs: string[], currentSelected : {}[], gridSize : string}) {
 
     return (
-        <button key={customKey} className={`${character} flex justify-center items-center 
+        <button key={customKey} className={`${character} flex justify-center items-center w-16 h-16
         ${clicked ? "bg-darkYellow pointer-events-none" : "bg-darkGray text-darkGray hover:bg-lightBlue2 hover:text-lightBlue2"} 
         ${correctPairs.includes(character.toString()) ? 'bg-lightGray cursor-not-allowed pointer-events-none ' : ''}  
         ${theme === 'numbers' && gridSize === '4x4' ? "text-xl p-4 " : theme === 'numbers' ? 'p-2' : theme === 'icons' && gridSize === '4x4' ? 'p-4' : 'p-2 text-xl'}
