4ffc76eaa20f3519104190ec47257e6dbb7c7ce4
jffryi <jefferyi.magtubo@gmail.com>
2024-04-03 15:39:27 -0500

    changed circle size on xl screens

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -5,7 +5,7 @@ export default function Circle({character, clicked, theme, changeClick, index, c
         <button key={customKey} className={`${character} flex justify-center items-center 
         ${clicked ? "bg-darkYellow pointer-events-none" : "bg-darkGray text-darkGray hover:bg-lightBlue2 hover:text-lightBlue2"} 
         ${correctPairs.includes(character.toString()) ? 'bg-lightGray cursor-not-allowed pointer-events-none ' : ''}  
-        ${theme === 'numbers' && gridSize === '4x4' ? "text-xl h-16 w-16 md:h-20 md:w-20 lg:w-24 lg:h-24" : theme === 'numbers' ? 'p-2 text-xl w-10 h-10 sm:w-16 sm:h-16 ' : theme === 'icons' && gridSize === '4x4' ? 'h-16 w-16 md:h-20 md:w-20 lg:w-24 lg:h-24' : 'p-2 text-xl w-10 h-10 sm:w-16 sm:h-16 '}
+        ${theme === 'numbers' && gridSize === '4x4' ? "text-xl h-16 w-16 md:h-20 md:w-20 lg:w-24 lg:h-24 xl:w-28 xl:h-28" : theme === 'numbers' ? 'p-2 text-xl w-10 h-10 sm:w-16 sm:h-16 ' : theme === 'icons' && gridSize === '4x4' ? 'h-16 w-16 md:h-20 md:w-20 lg:w-24 lg:h-24 xl:w-28 xl:h-28' : 'p-2 text-xl w-10 h-10 sm:w-16 sm:h-16  '}
         ${currentSelected.length === 2 ? 'pointer-events-none' : ''}
         rounded-full`}
         onClick={() => changeClick(index)}>
